78e24ab5632a5a61b731c727ef071a5f2a3ae643
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2022-05-19 02:42:57 -0300

    feat: create CRUD to TODO APP

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {TodoModule} from './app/todo/todo.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', process.env.DB_HOST),
+        port: configService.get('DB_PORT', +process.env.DB_PORT),
+        username: configService.get('DB_USERNAME', process.env.DB_USERNAME),
+        password: configService.get('DB_PASSWORD', process.env.DB_PASSWORD),
+        database: configService.get('DB_DATABASE', process.env.DB_DATABASE),
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+        synchronize: true,
+      }),
+    }),
+    TodoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
